eed0de84cb1e806ab9cfd125c63106568125278e
Noah Shine <shangzongyu1111@163.com>
2018-09-08 10:55:20 +0800

    update twosum use python

---
+++ algorithms/go/twoSum/twoSum.go
@@ -0,0 +1,33 @@
+// Source : https://oj.leetcode.com/problems/two-sum/
+// Author : Noah Shine
+// Date   : 2018-09-08
+
+/**********************************************************************************
+*
+* Given an array of integers, find two numbers such that they add up to a specific target number.
+*
+* The function twoSum should return indices of the two numbers such that they add up to the target,
+* where index1 must be less than index2. Please note that your returned answers (both index1 and index2)
+* are not zero-based.
+*
+* You may assume that each input would have exactly one solution.
+*
+* Input: numbers={2, 7, 11, 15}, target=9
+* Output: index1=1, index2=2
+*
+*
+**********************************************************************************/
+
+package twoSum
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{nums[i], nums[j]}
+			}
+		}
+	}
+
+	return nil
+}
---
+++ algorithms/go/twoSum/twoSum_test.go
@@ -0,0 +1,27 @@
+package twoSum
+
+import "testing"
+
+func Test_TwoSum_5nums(t *testing.T) {
+	var nums []int
+	for i := 0; i < 5; i++ {
+		nums = append(nums, i+1)
+	}
+	t.Log(twoSum(nums, 9))
+}
+
+func Test_TwoSum_50nums(t *testing.T) {
+	var nums []int
+	for i := 0; i < 50; i++ {
+		nums = append(nums, i)
+	}
+	t.Log(twoSum(nums, 90))
+}
+
+func Test_TwoSum_500nums(t *testing.T) {
+	var nums []int
+	for i := 0; i < 500; i++ {
+		nums = append(nums, i)
+	}
+	t.Log(twoSum(nums, 900))
+}
